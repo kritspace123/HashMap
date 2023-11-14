8484a0aacb1d003557411e6923cedc04e6b18701
kritspace123 <kritspace123@gmail.com>
2023-11-14 09:45:44 +0700

    PreRaddy

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <functional>
 
+#define M 11
+
 using namespace std;
 
 template <typename K, typename V>
@@ -17,12 +19,29 @@ private:
     
     Node** m_Table;
     int m_Size; /// количество элементов
-    int m_Cou; ///счетчик уникальных элементов
     int m_Capacity; ///Размер массива хэштаблицы
     double m_LoadFactor = 0.75;
     hash<K> Hasher;
 
-    void Resize() { ///Функция для увеличения размера хеш-таблицы
+
+
+public:
+    HashMap() : m_Size(0),  m_Capacity(M){
+        m_Table = new Node*[m_Capacity]();
+    }
+
+    ~HashMap() {
+        for (int i = 0; i < m_Capacity; ++i) {
+            Node* m_Indicator = m_Table[i];
+            while (m_Indicator != nullptr) {
+                Node* Next = m_Indicator->m_Next;
+                delete m_Indicator;
+                m_Indicator = Next;
+            }
+        }
+        delete[] m_Table;
+    }
+    void Resize(){
         int newCapacity = m_Capacity * 2;
         Node** m_NewTable = new Node*[newCapacity];
         for (int i = 0; i < m_Capacity; ++i) {
@@ -39,27 +58,10 @@ private:
         delete[] m_Table;
         m_Table = m_NewTable;
         m_Capacity = newCapacity;
-    }
-
-public:
-    HashMap() : m_Size(0), m_Cou(0), m_Capacity(31){
-        m_Table = new Node*[m_Capacity]();
-    }
-
-    ~HashMap() {
-        for (int i = 0; i < m_Capacity; ++i) {
-            Node* m_Indicator = m_Table[i];
-            while (m_Indicator != nullptr) {
-                Node* next = m_Indicator->m_Next;
-                delete m_Indicator;
-                m_Indicator = next;
-            }
-        }
-        delete[] m_Table;
-    }
+    }///Функция для увеличения размера хеш-таблицы
 
     V ElementSeatch(const K& key){
-        int index = Hasher(key)/m_Capacity;
+        int index = Hasher(key)%m_Capacity;
         Node* Indicator = m_Table[index];
 
         while(Indicator != nullptr){
@@ -92,13 +94,13 @@ public:
 
         /// вставляет новый элемент в хеш-таблицу
         Node* newNode = new Node(key, value);
-        ++m_Size;
+
         if (prev != nullptr) {
             prev->m_Next = newNode;
         } else {
             m_Table[index] = newNode;
         }
-        ++m_Cou;
+        ++m_Size;
     } ///Добавление нового элемента с заданными ключом и значением в коллекцию.
 
     void remove(const K& key) { ///удаления элемента из хеш-таблицы
@@ -115,7 +117,6 @@ public:
                 }
                 delete m_Indicator;
                 --m_Size;
-                --m_Cou;
                 return;
             }
             prev = m_Indicator;
@@ -123,15 +124,118 @@ public:
         }
     } /// Удаление из коллекции элемента с заданным ключом.
 
+    void PrintMap(){
+        Node* m_Indicator;
+        for(int i = 0; i < m_Capacity; i++){
+            cout << i << ": ";
+            m_Indicator = m_Table[i];
+            while(m_Indicator != nullptr){
+                cout << m_Indicator->m_Value << " ";
+                m_Indicator = m_Indicator->m_Next;
+            }
+            cout << endl;
+        }
+
+    }
+
+    // Iterator class
+    class Iterator {
+    private:
+        const HashMap& m_Map;
+        int m_Index;
+        Node* m_Indicator;
+
+    public:
+        Iterator(const HashMap& HashMap) : m_Map(HashMap), m_Index(0), m_Indicator(nullptr) {
+            MoveToTheNext();
+        }
+        Iterator(const HashMap& HashMap, int Index, Node* Indicator) : m_Map(HashMap), m_Index(Index), m_Indicator(Indicator) {}
+
+        bool HasNext() const {
+            return m_Indicator != nullptr;
+        }
+
+        void Next() {
+            m_Indicator = m_Indicator->m_Next;
+            if (!m_Indicator) {
+                m_Index++;
+                MoveToTheNext();
+            }
+        }
+
+        const K& getKey() const {
+            return m_Indicator->m_Key;
+        }
+
+        const V& getValue() const {
+            return m_Indicator->m_Value;
+        }
+
+    private:
+        void MoveToTheNext() {
+            while (m_Index < m_Map.m_Capacity && m_Map.m_Table[m_Index] == nullptr) {
+                m_Index++;
+            }
+
+            if (m_Index < m_Map.m_Capacity) {
+                m_Indicator = m_Map.m_Table[m_Index];
+            } else {
+                m_Indicator = nullptr;
+            }
+        }
+
+    };
+    Iterator GetIterator() const {
+        return Iterator(*this);
+    }
+
+    Iterator Begin() const {
+        int index = 0;
+        while (index < m_Capacity && m_Table[index] == nullptr) {
+            index++;
+        }
+        if (index < m_Capacity) {
+            return Iterator(*this, index, m_Table[index]);
+        } else {
+            return End();
+        }
+    }
+    Iterator End() const {
+        return Iterator(*this, m_Capacity, nullptr);
+    }
+
     int getTotalSize() const {
         return m_Size;
     }
 
-    int getUniqueSize() const {///???
-        return m_Cou;
-    }
+    int getUniqueSize() const {
+        int count = 0;
+
+        auto it = this->Begin();
+        auto EndIt = this->End();
+        V* ContainerValues = new V[m_Size];
+        while (it.HasNext()) {
+            V value =it.getValue();
+            int flag = 1;
+            for (int i = 0; i < count; ++i) {
+                if(ContainerValues[i] == value){
+                    flag = 0;
+                }
+            }
+            if(flag == 1){
+                ContainerValues[count] = value;
+                count++;
+            }
+            it.Next();
+        }
+        delete[] ContainerValues;
+        return count;
+    } ///Переписать так как сложность ухудшаертся
 
 };
+
+
+
 
 
 int main() {
@@ -144,15 +248,172 @@ int main() {
         return 1;
     }
 
-    char keyType, valueType;
+    char keyType, valueType, comand;
     F >> keyType >> valueType;
     int N;
     F >> N;
-    switch (keyType) {
-        case "I":
 
+    switch (keyType) {
+        case ('I'): {
+            int key;
+            switch (valueType) {
+                case 'I':{
+                    int value;
+                    HashMap<int, int> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;}
+                case 'D':{
+                    double value;
+                    HashMap<int, double> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;
+                }
+                case 'S': {
+                    string value;
+                    HashMap<int, string> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;
+                }
+            }
+            break;
+        }
+        case ('D'): {
+            double key;
+            switch (valueType) {
+                case 'I':{
+                    int value;
+                    HashMap<double, int> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;}
+                case 'D':{
+                    double value;
+                    HashMap<double, double> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;}
+                case 'S': {
+                    string value;
+                    HashMap<double, string> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;
+                }
+            }
+            break;
+        }
+        case ('S'): {
+            string key;
+            switch (valueType) {
+                case 'I':{
+                    int value;
+                    HashMap<string, int> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;}
+                case 'D':{
+                    double value;
+                    HashMap<string , double> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;
+                }
+                case 'S': {
+                    string value;
+                    HashMap<string, string> m_HashMap;
+                    for (int i = 0; i < N; ++i) {
+                        F >> comand;
+                        if (comand == 'A') {
+                            F >> key >> value;
+                            m_HashMap.Insert(key, value);
+                        } else {
+                            F >> key;
+                            m_HashMap.remove(key);
+                        }
+                    }
+                    cout << m_HashMap.getTotalSize() << " " << m_HashMap.getUniqueSize();
+                    break;
+                }
+            }
+            break;
+        }
     }
-    
-    Out << m_HasMap.getTotalSize() << " " << m_HasMap.getUniqueSize();
+
+
     return 0;
 }
